abf0f897628fd75bb9109dbe2d5f040f196fb50d
akarsh-shrivastava <akarsh.mails@rediffmail.com>
2019-02-01 23:19:44 +0530

    added get_xy

--- getdata.py
+++ getdata.py
@@ -1,3 +1,17 @@
+import dxl
+
+
+def init():
+    ports = dxl.get_available_ports()
+    print ("Available ports:", ports)
+    global arm
+    arm = dxl.dxl(a[0], 1000000)
+    motors = arm.scan(20)
+    print (motors)
+    if (len(motors)!=7)
+        raise IOError("All motors not detected")
+
+    print ("Connected to %s"%a[0])
 
 
 def get_angles(x,y):
@@ -5,11 +19,16 @@ def get_angles(x,y):
     return th1, th2
 
 def get_xy():
-    return 0, 0
+    th1 = arm.read(3, "Present Position")
+    th2 = arm.read(4, "Present Position")
+    return th1, th2
 
 if __name__ == '__main__':
+    init()
     file = "data.csv"
     fout = open(file, "w")
+    fout.write("x,y,th1,th2\n")
+    
     print ("Important!!!! use ^C to exit else the data wont be saved\n")
 
     while True:
